ef5068aa1014490f4d438b5de4526a9741b0acf3
ZH23320116 <zhuhe06@126.com>
2024-09-12 11:59:22 +0800

    First commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+    title: 'LongElkのBlog',
+    description: '',
+    base: '/LongElk.github.io/',
+})
