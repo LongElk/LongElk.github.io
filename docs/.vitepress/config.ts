85695a663745c5da412e1c11624b4c828edd10cf
ZH23320116 <zhuhe06@126.com>
2024-09-14 12:38:11 +0800

    fix: delete base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
     title: 'LongElkのBlog',
-    description: '',
-    base: '/MyBlog/',
+    description: ''
 })
