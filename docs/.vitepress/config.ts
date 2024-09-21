e5161c52cccd3df77d2cc0c4c7a17f3211680972
ZH23320116 <zhuhe06@126.com>
2024-09-21 10:40:49 +0800

    feat: a new update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,23 +4,21 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
     title: 'LongElkのBlog',
     description: '分享我的技术与生活-由vitepress搭建的个人博客',
-    head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }]
-      ],
     themeConfig: {
         siteTitle: 'LongElk',
-        logo: '../public/头像.svg',
+        logo: '/头像.svg',
         socialLinks: [
           { icon: 'github', link: 'https://github.com/longelk' }
         ],
-    
+        search: {
+          provider: 'local'
+        },
         // 导航栏
         nav: [
           { text: '首页', link: '/' },
           { text: '博客', link: '/blog/' },
           { text: '关于我', link: '/about' }
-        ],
-    
+        ],   
         // 侧边栏（以博客页面为例）
         sidebar: {
           '/blog/': [
@@ -33,11 +31,13 @@ export default defineConfig({
             }
           ]
         },
-    
         // 页脚
         footer: {
           message: '感谢访问我的博客',
           copyright: '版权所有 © 2024'
         }
-      }
+      },
+      head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
+      ]
 })
